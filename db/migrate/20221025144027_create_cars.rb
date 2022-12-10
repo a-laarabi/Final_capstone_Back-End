27c088cba58e909bb780942687cf192a538fd2f6
abdollahraafat <abdollah.raafat.daif@gmail.com>
2022-12-10 05:45:12 +0200

    Create cars models

--- db/migrate/20221025144027_create_cars.rb
+++ db/migrate/20221025144027_create_cars.rb
@@ -1,7 +1,12 @@
 class CreateCars < ActiveRecord::Migration[7.0]
   def change
     create_table :cars do |t|
-      t.string :name
+      t.string :model
+      t.string :brand
+      t.string :year
+      t.datetime :year_realeased
+      t.integer :user_id
+
       t.timestamps
     end
   end
