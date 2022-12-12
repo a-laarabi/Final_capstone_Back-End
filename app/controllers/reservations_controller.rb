9d0fc76e90a925df16e1a8c2d909ba47e8fced2d
abdollahraafat <abdollah.raafat.daif@gmail.com>
2022-12-12 08:06:09 +0200

    Fixed car delete bug

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,27 +1,38 @@
 class ReservationsController < ApplicationController
   def index
-    @reservations = current_user.reservations.includes(:car)
-    render json: { code: 200,
-                   reservations: @reservations }, status: :ok
+    if current_user
+      @reservations = current_user.reservations.includes(:car)
+      render json: { code: 200,
+                     reservations: @reservations }, status: :ok
+    else
+      render json: { code: 401,
+                     message: 'You need to sign in or sign up before continuing.' }, status: :unauthorized
+    end
   end
 
   def create
-    @reservation = current_user.reservations.create(reservation_params)
+    @reservation = current_user.reservations.new(reservation_params)
     if @reservation.save
       render json: { code: 200,
                      reservation: @reservation,
                      message: 'Reservation created' }, status: :ok
     else
       render json: { code: 400,
-                     message: 'Reservation not created' }, status: :bad_request
+                     message: 'Reservation not created',
+                     error: @reservation.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def destroy
     @reservation = current_user.reservations.find(params[:id])
-    @reservation.destroy
-    render json: { code: 200,
+    if @reservation.destroy
+      render json: { code: 200, reservation: @reservation,
                    message: 'Reservation deleted' }, status: :ok
+    else
+      render json: { code: 400, reservation: @reservation,
+                     message: 'Reservation not deleted',
+                     error: @reservation.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   private
