46f7d0efef31cc82f0348589befa0f56fd46b59b
Anasse LAARABI <contact.laarabi@gmail.com>
2022-12-13 22:33:02 +0100

    Update App

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,10 +1,5 @@
 class Car < ApplicationRecord
-    belongs_to :user
+  has_many :reservation, foreign_key: :car_id
 
-    validates :model, presence: true
-    validates :brand, presence: true
-    validates :year_realeased, presence: true
-    validates :user_id, presence: true
-    
-    has_many :reservation , foreign_key: 'car_id' , dependent: :destroy
+  validates :model, :brand, :year_released, :featured_image, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :reservations, foreign_key: :user_id, dependent: :delete_all
+  # whats dependent: :destroy?
+  # it means that if the user is deleted, the cars will be deleted as well
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  validates :name, presence: true
 end
